b3b7f171453bf246a075c45cee2dd9b481b4bc29
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-03-12 09:54:24 +0800

    Network in Network

---
+++ Network in Network/network_in_network.py
@@ -0,0 +1,11 @@
+#!/home/sunnymarkliu/software/miniconda2/bin/python
+# _*_ coding: utf-8 _*_
+
+"""
+Network in Network(NIN) model implementation example using TensorFlow library.
+
+NIN Paper: Network In Network(https://arxiv.org/abs/1312.4400)
+
+@author: MarkLiu
+@time  : 17-3-12 上午9:36
+"""
